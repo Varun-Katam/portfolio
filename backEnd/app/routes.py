636110f687d07d879c4c911da355d5d533d40612
Varun-Katam <varunabhi3327@gmail.com>
2026-08-30 13:26:49 +0530

    Used supabase database table

--- backEnd/app/routes.py
+++ backEnd/app/routes.py
@@ -1,6 +1,6 @@
 #routes.py
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.userSchema import UserCreate
 from app.users import send_message
@@ -8,6 +8,6 @@ from utils.db import get_db
 
 userRoutes = APIRouter()
 
-@userRoutes.post("/contact")
+@userRoutes.post("/contact", status_code = status.HTTP_201_CREATED)
 def send_data(user: UserCreate, db : Session = Depends(get_db)):
     return send_message(user, db)
